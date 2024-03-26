d9acd9d70e537889323f1abd44be9a36f6886094
bayskelt <rinognom@gmail.com>
2024-03-27 00:23:41 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace HomeTask_3_add_.NET
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введiть довiльне дiйсне число: ");
+            int number = int.Parse(Console.ReadLine());
+            int total = 0;
+
+            if (number < 0)
+            {
+                number *= -1;
+            }
+
+            while (number != 0)
+            {
+                total += number % 10;
+                number /= 10;
+            }
+
+            Console.Write($"Сума цифр введеного числа: {total}");
+        }
+    }
+}
